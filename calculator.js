bb42a390c8627dd1e0340855b13b44426385530a
keshavshinde888 <shindekeshav008@gmail.com>
2024-11-29 18:44:52 +0530

    Commit code done

--- calculator.js
+++ calculator.js
@@ -28,7 +28,7 @@ function add(numbers){
 
 console.log(add(",")) 
 console.log(add("1,5")) 
+console.log(add("//;\n1;299")) 
 console.log(add("1")) 
 console.log(add("1\n2,3")) 
-console.log(add("//;\n1;2")) 
 console.log(add("-1/9")) 
